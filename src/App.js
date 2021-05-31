bd0b9a8f8b355b0b832d4477af4c3ef263660ca3
DeveloperLVA <DeveloperLVA@gmail.com>
2021-05-31 21:53:52 +0300

    01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {useForm} from "react-hook-form";
 
 function App() {
+
+const {
+  register,
+  handleSubmit,
+  formState: {errors, submitCount},
+  watch
+
+} = useForm({mode:'onChange'});
+
+const onSubmit = data => console.log ('отправлено :', data);
+
+const name = watch ('name');
+const age = watch ('age');
+// console.log ('Имя' + name);
+// console.log ('Возраст' + age);
+
+//console.log (errors);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form" onSubmit={handleSubmit (onSubmit)}>
+      <h1 className="title">Форма</h1>
+
+      <input 
+        defaultValue="John"
+        {...register ('name', {required: true, maxLength: 15})}
+      />
+      {errors.name && <i>Обязательное поле - не более 15 символов</i>} 
+      <hr />
+      
+      <input      
+          {...register ('age', {required: true, pattern: /\d?\d/, min: 18, max: 99})}
+      /> 
+      {errors.age && <i>Обязательное числовое поле - от 18 до 99</i>}
+      <hr />
+
+      <select id="sel" {...register ('abbabalamaga')}>
+        <option value='yes'>Да</option>
+        <option value='no'>Нет</option>
+      </select>
+      <hr />
+
+      <input type="submit" />
+      Сколько раз отправлена ?  {submitCount}
+    </form>
   );
 }
 
